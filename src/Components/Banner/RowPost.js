77f5296837462f392e053dd61ba7e333bfa89c99
Ajaya Kumar <ajayakumar4741@gmail.com>
2025-07-02 18:36:48 +0530

    Poster trailer error display on ui

--- src/Components/Banner/RowPost.js
+++ src/Components/Banner/RowPost.js
@@ -5,12 +5,12 @@ import axios from './Axios'
 
 function RowPost(props) {
     const [movies, setMovies] = useState([])
-
+    const [noVideo, setNoVedio] = useState(false)
     useEffect(() => {
         axios.get(props.urls).then((response) => {
             setMovies(response.data.results)
         })
-     }, [props.urls])
+    }, [props.urls])
 
     const handleMovie = (id) => {
         console.log(id)
@@ -19,22 +19,30 @@ function RowPost(props) {
                 const videoKey = response.data.results[0].key
                 const youtubeUrl = `https://www.youtube.com/watch?v=${videoKey}`
                 window.open(youtubeUrl, '_blank')
-                } else {
-                console.log("No videos found for this movie.");
-            }
-        })
+            } else {
+                setNoVedio(true);
+            }})
+            .catch(error => {
+                console.error('Error fetching video:', error)
+                setNoVedio(true) // Show fallback message on error too
+            })
     }
     return (
         <div className='row'>
             <h2>{props.title}</h2>
             <div className='posters'>
                 {movies.map((obj) => {
-                    return ( <img onClick={() => { handleMovie(obj.id) }} className={props.isSmall ? 'small-poster' : 'poster'} src={`${imageUrl + obj.backdrop_path}`} alt="" />
-                        
+                    return (<img onClick={() => { handleMovie(obj.id) }} className={props.isSmall ? 'small-poster' : 'poster'} src={`${imageUrl + obj.backdrop_path}`} alt="" />
+
                     )
                 })}
 
             </div>
+            {noVideo && (
+                <p className="no-video-message">
+                    Sorry, no trailer available for this movie.
+                </p>
+            )}
 
         </div>
     )
